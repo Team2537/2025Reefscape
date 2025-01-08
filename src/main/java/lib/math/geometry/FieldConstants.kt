1ef6ae84fcdeb7640e69a4126982b7fa1aac9299
falon <falon.n.clark@gmail.com>
2025-01-08 16:47:29 -0500

    fix node poses, refactor node selection so that poses are created and cached on init

--- src/main/java/lib/math/geometry/FieldConstants.kt
+++ src/main/java/lib/math/geometry/FieldConstants.kt
@@ -4,21 +4,17 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Pose3d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Rotation3d
-import edu.wpi.first.math.geometry.Transform2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.geometry.Translation3d
 import edu.wpi.first.math.util.Units
-import edu.wpi.first.units.Units.Centimeter
 import edu.wpi.first.units.Units.Inches
 import edu.wpi.first.units.Units.Meters
-import edu.wpi.first.units.Units.Rotation
 import edu.wpi.first.units.measure.Distance
 import lib.math.units.centi
 import lib.math.units.inches
 import lib.math.units.into
 import lib.math.units.measuredIn
 import lib.math.units.meters
-import org.littletonrobotics.junction.Logger
 
 object FieldConstants {
     val fieldWidth = (26.0 * 12.0) + 5.0 measuredIn Inches
@@ -38,17 +34,17 @@ object FieldConstants {
     }
     
     enum class Level(val height: Distance, val angle: Double, val radius: Distance) {
-        L1(46.0.centi.meters, 35.0, 29.421141.inches),
-        L2(81.0.centi.meters, 35.0, 29.421141.inches),
-        L3(121.0.centi.meters, 35.0, 29.421141.inches),
-        L4(183.0.centi.meters, 90.0, 29.421141.inches)
+        L1(46.0.centi.meters, 35.0, 30.679296.inches),
+        L2(31.218618.inches, 35.0, 30.679296.inches),
+        L3(47.088688.inches, 35.0, 30.679296.inches),
+        L4(71.994600.inches, 90.0, 30.738196.inches)
     }
     
     enum class ReefFace {
         S1, S2, S3, S4, S5, S6
     }
     
-    fun getNodePose(level: Level, side: ReefFace, rack: Side): Pose3d {
+    private fun calcNodePose(level: Level, side: ReefFace, rack: Side): Pose3d {
         var x = reefCenterBlue.x.meters - level.radius
         var y = reefCenterBlue.y.meters
         
@@ -74,17 +70,18 @@ object FieldConstants {
         )
     }
     
-    val nodes: List<List<Pose3d>> = ReefFace.values().map { reefFace ->
-        Side.values().flatMap { side ->
-            Level.values().map { level ->
-                getNodePose(level, reefFace, side)
+    data class Node(val pose: Pose3d, val side: Side, val level: Level, val reefFace: ReefFace)
+    
+    val nodes: List<Node> = Level.values().flatMap { level ->
+        ReefFace.values().flatMap { reefFace ->
+            Side.values().map { side ->
+                Node(calcNodePose(level, reefFace, side), side, level, reefFace)
             }
         }
     }
     
-    init {
-        nodes.forEachIndexed { i, list ->
-            Logger.recordOutput("S${i + 1} Nodes", *list.toTypedArray())
-        }
+    fun getNode(level: Level, reefFace: ReefFace, side: Side): Node {
+        return nodes.first { it.level == level && it.reefFace == reefFace && it.side == side }
     }
+
 }
