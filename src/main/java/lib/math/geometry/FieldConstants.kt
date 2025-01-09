608fdfd87ee5b85e5dd897ce1c70da35016a42a2
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2025-01-09 18:46:50 -0500

    Convert many functions to properties

--- src/main/java/lib/math/geometry/FieldConstants.kt
+++ src/main/java/lib/math/geometry/FieldConstants.kt
@@ -10,14 +10,10 @@ import edu.wpi.first.math.geometry.Transform2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.geometry.Translation3d
 import edu.wpi.first.math.util.Units
-import edu.wpi.first.units.Units.Inches
-import edu.wpi.first.units.Units.Meters
+import edu.wpi.first.units.Units.*
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
-import lib.math.units.centi
-import lib.math.units.inches
-import lib.math.units.into
-import lib.math.units.measuredIn
-import lib.math.units.meters
+import lib.math.units.*
 import org.littletonrobotics.junction.Logger
 
 /**
@@ -28,13 +24,13 @@ import org.littletonrobotics.junction.Logger
 object FieldConstants {
     
     /** Unit safe field width */
-    val fieldWidth = (26.0 * 12.0) + 5.0 measuredIn Inches
+    val width = (26.0 * 12.0) + 5.0 measuredIn Inches
     
     /** Unit safe field length */
-    val fieldLength = (57.0 * 12.0) + 6.0 + 7.0 / 8 measuredIn Inches
+    val length = (57.0 * 12.0) + 6.0 + 7.0 / 8 measuredIn Inches
     
     /** Center of the field */
-    val fieldCenter = Pose2d(fieldLength / 2.0, fieldWidth / 2.0, Rotation2d())
+    val center = Pose2d(length / 2.0, width / 2.0, Rotation2d())
     
     // Access the reef and processor constants to initialize them
     init {
@@ -46,15 +42,15 @@ object FieldConstants {
     object Reef {
         
         /** Center of the blue reef */
-        val reefCenterBlue =
+        val blueCenter =
             Translation2d(
                 Meters.of(4.489325),
                 Meters.of(4.025877)
             )
         
         /** Circle centered around the reef, expanding 20 inches past its edge */
-        val reefZone: Ellipse2d = Ellipse2d(
-            reefCenterBlue,
+        val zone: Ellipse2d = Ellipse2d(
+            blueCenter,
             32.745545.inches + 20.inches,
         )
         
@@ -75,11 +71,11 @@ object FieldConstants {
          *
          * @see [calcNodePose]
          */
-        enum class Level(val height: Distance, val angle: Double, val radius: Distance) {
-            L1(46.0.centi.meters, 35.0, 30.679296.inches),
-            L2(31.218618.inches, 35.0, 30.679296.inches),
-            L3(47.088688.inches, 35.0, 30.679296.inches),
-            L4(71.994600.inches, 90.0, 30.738196.inches)
+        enum class Level(val height: Distance, val angle: Angle, val radius: Distance) {
+            L1(46.0.centi.meters, 35.0.degrees, 30.679296.inches),
+            L2(31.218618.inches, 35.0.degrees, 30.679296.inches),
+            L3(47.088688.inches, 35.0.degrees, 30.679296.inches),
+            L4(71.994600.inches, 90.0.degrees, 30.738196.inches)
         }
         
         /**
@@ -87,7 +83,7 @@ object FieldConstants {
          *
          * S1 is the face closest to the driver station, continues CCW around the reef
          */
-        enum class ReefFace {
+        enum class Face {
             S1, S2, S3, S4, S5, S6
         }
         
@@ -100,9 +96,9 @@ object FieldConstants {
          *
          * @return The pose of the node
          */
-        private fun calcNodePose(level: Level, side: ReefFace, rack: Side): Pose3d {
-            var x = reefCenterBlue.x.meters - level.radius
-            var y = reefCenterBlue.y.meters
+        private fun calcNodePose(level: Level, side: Face, rack: Side): Pose3d {
+            var x = blueCenter.x.meters - level.radius
+            var y = blueCenter.y.meters
             
             when (rack) {
                 Side.LEFT -> y -= Inches.of(12.937968 / 2)
@@ -114,13 +110,13 @@ object FieldConstants {
             val sideDelta = Rotation2d.fromDegrees(60.0).times(side.ordinal.toDouble())
             
             translation2d = Translation2d(
-                (sideDelta.cos * (translation2d.x - reefCenterBlue.x)) - (sideDelta.sin * (translation2d.y - reefCenterBlue.y)) + reefCenterBlue.x,
-                (sideDelta.sin * (translation2d.x - reefCenterBlue.x)) + (sideDelta.cos * (translation2d.y - reefCenterBlue.y)) + reefCenterBlue.y
+                (sideDelta.cos * (translation2d.x - blueCenter.x)) - (sideDelta.sin * (translation2d.y - blueCenter.y)) + blueCenter.x,
+                (sideDelta.sin * (translation2d.x - blueCenter.x)) + (sideDelta.cos * (translation2d.y - blueCenter.y)) + blueCenter.y
             )
             
             return Pose3d(
                 Translation3d(translation2d.x, translation2d.y, level.height into Meters),
-                Rotation3d(0.0, Units.degreesToRadians(level.angle), sideDelta.radians)
+                Rotation3d(0.0, level.angle into Radians, sideDelta.radians)
             )
         }
         
@@ -130,14 +126,14 @@ object FieldConstants {
          * @param pose The pose of the node
          * @param side The side of the reef
          * @param level The level of the reef
-         * @param reefFace The face of the reef
+         * @param face The face of the reef
          */
-        data class Node(val pose: Pose3d, val side: Side, val level: Level, val reefFace: ReefFace)
+        data class Node(val pose: Pose3d, val side: Side, val level: Level, val face: Face)
         
         /** List of all nodes on the reef */
-        private val nodes: List<Node> = Level.values().flatMap { level ->
-            ReefFace.values().flatMap { reefFace ->
-                Side.values().map { side ->
+        private val nodes: List<Node> = Level.entries.flatMap { level ->
+            Face.entries.flatMap { reefFace ->
+                Side.entries.map { side ->
                     Node(calcNodePose(level, reefFace, side), side, level, reefFace)
                 }
             }
@@ -147,25 +143,25 @@ object FieldConstants {
          * Gets a node on the reef
          *
          * @param level The level of the reef
-         * @param reefFace The face of the reef
+         * @param face The face of the reef
          * @param side The side of the reef
          *
          * @return The node
          * @see Node
          * @throws NoSuchElementException if the node does not exist
          */
-        fun getNode(level: Level, reefFace: ReefFace, side: Side): Node {
-            return nodes.first { it.level == level && it.reefFace == reefFace && it.side == side }
+        fun getNode(level: Level, face: Face, side: Side): Node {
+            return nodes.first { it.level == level && it.face == face && it.side == side }
         }
         
         init {
-            Logger.recordOutput("field/reef/reefCenterBlue", reefCenterBlue)
-            Logger.recordOutput("field/reef/reefZone", *reefZone.cardinals().toTypedArray())
+            Logger.recordOutput("field/reef/reefCenterBlue", blueCenter)
+            Logger.recordOutput("field/reef/reefZone", *zone.cardinals.toTypedArray())
         }
     }
     
     object Processor {
-        val processorHoleCenter = Pose3d(
+        val holeCenter = Pose3d(
             Translation3d(
                 (221.726104.inches) + (14.0.inches),
                 Inches.zero(),
@@ -173,17 +169,18 @@ object FieldConstants {
             ),
             Rotation3d()
         )
-        
-        val processorZone: Rectangle2d =
+
+        val zone: Rectangle2d =
             Rectangle2d(
-                processorHoleCenter.toPose2d().plus(Transform2d(Inches.zero(), 15.0.inches, Rotation2d())),
+                holeCenter.toPose2d().plus(Transform2d(Inches.zero(), 15.0.inches, Rotation2d())),
                 50.inches,
                 30.0.inches,
             )
         
         init {
-            Logger.recordOutput("field/processor/processorHoleCenter", processorHoleCenter)
-            Logger.recordOutput("field/processor/processorZone", *processorZone.corners().toTypedArray())
+            Logger.recordOutput("field/processor/holeCenter", holeCenter)
+            // Log corners to display them; default rect2d logging is less intuitive for this
+            Logger.recordOutput("field/processor/zone", *zone.corners.toTypedArray())
         }
         
     }
@@ -227,7 +224,7 @@ object FieldConstants {
         
         init {
             Logger.recordOutput("field/barge/cages", cage1, cage2, cage3)
-            Logger.recordOutput("field/barge/cageZone", *cageZone.corners().toTypedArray())
+            Logger.recordOutput("field/barge/cageZone", *cageZone.corners.toTypedArray())
         }
     }
 }
--- src/main/java/lib/math/geometry/FieldFlipUtils.kt
+++ src/main/java/lib/math/geometry/FieldFlipUtils.kt
@@ -7,31 +7,34 @@ import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.geometry.Translation3d
 
-fun Translation2d.flip(): Translation2d {
-    return Translation2d(
-        2 * FieldConstants.fieldCenter.translation.x - this.x,
-        2 * FieldConstants.fieldCenter.translation.y - this.y
-    )
-}
+val Translation2d.flipped: Translation2d
+    get() {
+        return Translation2d(
+            2 * FieldConstants.center.translation.x - this.x,
+            2 * FieldConstants.center.translation.y - this.y
+        )
+    }
 
-fun Pose2d.flip(): Pose2d {
-    return Pose2d(
-        this.translation.flip(),
-        this.rotation.rotateBy(Rotation2d.fromDegrees(180.0))
-    )
-}
+val Pose2d.flipped: Pose2d
+    get() {
+        return Pose2d(
+            this.translation.flipped,
+            this.rotation.rotateBy(Rotation2d.fromDegrees(180.0))
+        )
+    }
 
-fun Translation3d.flip(): Translation3d {
-    return Translation3d(
-        2 * FieldConstants.fieldCenter.translation.x - this.x,
-        2 * FieldConstants.fieldCenter.translation.y - this.y,
-        this.z
-    )
-}
+val Translation3d.flipped: Translation3d
+    get() {
+        return Translation3d(
+            2 * FieldConstants.center.translation.x - this.x,
+            2 * FieldConstants.center.translation.y - this.y,
+            this.z
+        )
+    }
 
-fun Pose3d.flip(): Pose3d {
+fun Pose3d.flipped(): Pose3d {
     return Pose3d(
-        this.translation.flip(),
+        this.translation.flipped,
         this.rotation.rotateBy(
             Rotation3d(Rotation2d.fromDegrees(180.0))
         )
