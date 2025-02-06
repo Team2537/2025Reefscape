d5614a09da562bb11e08e915e2f409c2a3a40c9d
Matthew <84196639+mteam88@users.noreply.github.com>
2025-02-05 19:20:24 -0500

    made requested changes

--- src/main/java/frc/robot/subsystems/climb/ClimbIOHardware.kt
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOHardware.kt
@@ -1,37 +1,57 @@
 package frc.robot.subsystems.climb
 
-import edu.wpi.first.units.Measure
+import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkBase
+import com.revrobotics.spark.config.SparkMaxConfig
+import com.revrobotics.spark.config.SparkBaseConfig
 import edu.wpi.first.units.Units.*
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.Voltage
+import kotlin.math.PI
 
-class ClimbIOHardware (
+class ClimbIOHardware(
     private val id: Int,
     private val isInverted: Boolean,
     private val gearing: Double,
-    private val drumRadius: Meters,
-    ): ClimberArmIO {
-        private val motor: CANSparkMax = CanSparkMax (id, CanSparkLowLevel.MotorType.kBrushless).apply {
-            inverted = isInverted
-            encoder.angularVelocityConversionFactor = 1/gearing
-            idleMode = CANSparkBase.IdleMode.kBrake
-        }
-    override fun updateInputs(inputs: ClimberArmIO.ClimberArmInputs) {
-        inputs.angleFromAbsoluteEncoder.mut_replace()
+) : ClimbIO {
+    private val brakeConfig = SparkMaxConfig().apply {
+        inverted(isInverted)
+        encoder.positionConversionFactor(1.0 / gearing)
+        encoder.velocityConversionFactor(1.0 / gearing)
+        idleMode(SparkBaseConfig.IdleMode.kBrake)
+        smartCurrentLimit(30)
+    }
+
+    private val coastConfig = brakeConfig.idleMode(SparkBaseConfig.IdleMode.kCoast)
+
+    private val motor = SparkMax(id, SparkLowLevel.MotorType.kBrushless).apply {
+        configure(brakeConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+    }
+
+    override fun updateInputs(inputs: ClimbIO.ClimbArmInputs) {
+        inputs.absoluteAngle.mut_replace(Rotations.of(motor.encoder.position))
         inputs.angularVelocity.mut_replace(
-            motor.encoder.angularVelocity / 60) * (drumRadius into Meters) * 2 * Math.Pi,
-        MetersPerSecond)
-        inputs.supplyVoltage.mut_replace()
-        inputs.motorVoltage.mut_replace()
+            RotationsPerSecond.of(motor.encoder.velocity / 60.0)
+        )
+        inputs.supplyVoltage.mut_replace(Volts.of(motor.busVoltage))
+        inputs.motorVoltage.mut_replace(Volts.of(motor.appliedOutput * motor.busVoltage))
         inputs.statorCurrent.mut_replace(Amps.of(motor.outputCurrent))
     }
-    override fun setVoltage(voltage: Volts, isClosedLoop: Boolean {
-        motor.setVoltage(voltage into Volts)
+
+    override fun setVoltage(voltage: Voltage) {
+        motor.setVoltage(voltage.baseUnitMagnitude())
     }
-    override fun setBrakeMode() {
+
+    override fun setBrakeMode(brakeEnabled: Boolean) {
+        motor.configure(
+            if(brakeEnabled) brakeConfig else coastConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
     }
+
     override fun stop() {
         motor.stopMotor()
     }
-
 }
