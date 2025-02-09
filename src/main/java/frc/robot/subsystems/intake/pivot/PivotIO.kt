4388f1248cb4fb18fcf52a9137d737c3e4b01c0d
falOn-Dev <falon.n.clark@gmail.com>
2025-02-09 11:47:35 -0500

    Split intake io, create pivot io

---
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIO.kt
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.intake.pivot
+
+import edu.wpi.first.units.Units.*
+import edu.wpi.first.units.measure.*
+import org.littletonrobotics.junction.LogTable
+import org.littletonrobotics.junction.inputs.LoggableInputs
+
+interface PivotIO {
+    class PivotInputs: LoggableInputs {
+        val position: MutAngle = Radians.zero().mutableCopy()
+        val velocity: MutAngularVelocity = RadiansPerSecond.zero().mutableCopy()
+        val appliedVoltage: MutVoltage = Volt.zero().mutableCopy()
+        val statorCurrent: MutCurrent = Amps.zero().mutableCopy()
+
+        override fun toLog(table: LogTable) {
+            table.put("position", position)
+            table.put("velocity", velocity)
+            table.put("appliedVoltage", appliedVoltage)
+            table.put("statorCurrent", statorCurrent)
+        }
+
+        override fun fromLog(table: LogTable) {
+            position.mut_replace(table.get("position", position))
+            velocity.mut_replace(table.get("velocity", velocity))
+            appliedVoltage.mut_replace(table.get("appliedVoltage", appliedVoltage))
+            statorCurrent.mut_replace(table.get("statorCurrent", statorCurrent))
+        }
+    }
+
+    /** Poll the sensors to update the passed in inputs object */
+    fun updateInputs(inputs: PivotInputs) {}
+
+    fun setVoltage(volts: Voltage) {}
+
+    fun setTargetAngle(angle: Angle) {}
+}
