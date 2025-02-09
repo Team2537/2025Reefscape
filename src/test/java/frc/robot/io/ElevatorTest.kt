5e401c91e696c0e70716334f1675cd6b80924be7
Falon <47285315+falOn-Dev@users.noreply.github.com>
2025-02-09 15:48:56 -0500

    Elevator sim IO impl (#39)
    * add in elevator sim boilerplate
    
    * Finsiehd elevator IO sim
    
    * Elevator tests

---
+++ src/test/java/frc/robot/io/ElevatorTest.kt
@@ -0,0 +1,46 @@
+package frc.robot.io
+
+import edu.wpi.first.hal.HAL
+import edu.wpi.first.units.Units.*
+import edu.wpi.first.units.measure.Voltage
+import frc.robot.subsystems.elevator.ElevatorIO
+import frc.robot.subsystems.elevator.ElevatorIOSim
+import lib.controllers.gains.FeedforwardGains
+import lib.controllers.gains.PIDGains
+import lib.math.units.inches
+import lib.math.units.into
+import lib.math.units.measuredIn
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ElevatorTest {
+    var io: ElevatorIO? = null
+    var inputs: ElevatorIO.ElevatorInputs? = null
+
+    @BeforeEach
+    fun setUp() {
+        assert(HAL.initialize(500, 0))
+
+        io = ElevatorIOSim(
+            gearing = 4.0,
+            drumRadius = Inches.of(0.88),
+            pidGains = PIDGains(),
+            ffGains = FeedforwardGains(kV = 1.77),
+            kG = 0.19,
+            mass = 15.0 measuredIn Pounds
+        )
+
+        inputs = ElevatorIO.ElevatorInputs()
+    }
+
+    @Test
+    fun `Elevator moves up`(){
+        io!!.setElevatorVoltage(6.0 measuredIn Volts)
+        for(i in 0..2000){
+            io!!.updateInputs(inputs!!)
+        }
+
+        assert(inputs!!.carriageHeight > 0.0.inches)
+    }
+}
