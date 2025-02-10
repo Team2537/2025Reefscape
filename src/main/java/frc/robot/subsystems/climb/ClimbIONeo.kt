bc7eef7e54c181c01c4429501d1c5e4ce67ef070
Matthew <84196639+mteam88@users.noreply.github.com>
2025-02-10 18:33:50 -0500

    Refactor ClimbIONeo brake mode configuration

--- src/main/java/frc/robot/subsystems/climb/ClimbIONeo.kt
+++ src/main/java/frc/robot/subsystems/climb/ClimbIONeo.kt
@@ -15,18 +15,16 @@ class ClimbIONeo(
     private val isInverted: Boolean,
     private val gearing: Double,
 ) : ClimbIO {
-    private val brakeConfig = SparkMaxConfig().apply {
+    private val coastConfig = SparkMaxConfig().apply {
         inverted(isInverted)
         encoder.positionConversionFactor(1.0 / gearing)
         encoder.velocityConversionFactor(1.0 / gearing)
-        idleMode(SparkBaseConfig.IdleMode.kBrake)
+        idleMode(SparkBaseConfig.IdleMode.kCoast)
         smartCurrentLimit(30)
     }
 
-    private val coastConfig = brakeConfig.idleMode(SparkBaseConfig.IdleMode.kCoast)
-
     private val motor = SparkMax(id, SparkLowLevel.MotorType.kBrushless).apply {
-        configure(brakeConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+        configure(coastConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
     }
 
     override fun updateInputs(inputs: ClimbIO.ClimbArmInputs) {
@@ -44,11 +42,9 @@ class ClimbIONeo(
     }
 
     override fun setBrakeMode(brakeEnabled: Boolean) {
-        motor.configure(
-            if(brakeEnabled) brakeConfig else coastConfig,
-            SparkBase.ResetMode.kResetSafeParameters,
-            SparkBase.PersistMode.kPersistParameters
-        )
+        val brakeConfig = SparkMaxConfig()
+        brakeConfig.idleMode(if(brakeEnabled) SparkBaseConfig.IdleMode.kBrake else SparkBaseConfig.IdleMode.kCoast)
+        motor.configure(brakeConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
     }
 
     override fun stop() {
