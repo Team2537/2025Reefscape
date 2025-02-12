20d146ea7f17422684d7993e2335b30358943f9b
falon <falon.n.clark@gmail.com>
2025-02-12 00:02:34 -0500

    Switch to unique setpoint enum, and fix command issues

--- src/main/java/frc/robot/subsystems/elevator/Elevator.kt
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.elevator
 
 import edu.wpi.first.units.Units.Inches
 import edu.wpi.first.units.Units.Pounds
+import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -16,6 +17,19 @@ import org.littletonrobotics.junction.Logger
 import kotlin.math.max
 
 class Elevator : SubsystemBase("elevator") {
+    
+    /**
+     * The setpoints to send the elevator to when scoring on the reef
+     * Differs from [FieldConstants.Reef.Level] as that is the actual reef height, this is the elevator height to score on the reef
+     * @param height The height to send the elevator to
+     */
+    enum class Setpoint(val height: Distance) {
+        HOME(0.0.inches),
+        L1(5.0.inches),
+        L2(15.0.inches),
+        L3(30.0.inches),
+        L4(45.0.inches),
+    }
     
     val io: ElevatorIO = when (RobotType.mode) {
         RobotType.Mode.SIMULATION -> ElevatorIOSim(
@@ -33,47 +47,43 @@ class Elevator : SubsystemBase("elevator") {
     
     val inputs: ElevatorIO.ElevatorInputs = ElevatorIO.ElevatorInputs()
     
-    private var currentNodeHeight: FieldConstants.Reef.Level? = null
+    private var currentSetpoint = Setpoint.HOME
     
     fun getSendToNodeCmd(node: FieldConstants.Reef.Level): Command {
         currentNodeHeight = node
         return Commands.print("Sending to node $node")
             .andThen(runOnce { io.setElevatorHeightTarget(node.height) })
+    fun getSendToNodeCmd(setpoint: Setpoint): Command {
+        return runOnce {
+            currentSetpoint = setpoint
+            io.setElevatorHeightTarget(setpoint.height)
+        }
     }
     
     fun getMoveUpCmd(): Command {
-        val levels = FieldConstants.Reef.Level.entries.sortedBy { it.height } // Ensure levels are sorted by height.
-        
-        if (currentNodeHeight == null) {
-            return getSendToNodeCmd(levels.first())
+        return runOnce {
+            currentSetpoint = Setpoint.entries.find { it.height > currentSetpoint.height } ?: currentSetpoint
+            io.setElevatorHeightTarget(currentSetpoint.height)
         }
-        
-        val nextLevel = levels.find { it.height > currentNodeHeight!!.height }
-        return nextLevel?.let { getSendToNodeCmd(it) } ?: Commands.idle(this)
     }
     
     fun getMoveDownCmd(): Command {
-        val levels = FieldConstants.Reef.Level.entries.sortedBy { it.height } // Ensure levels are sorted by height.
-        
-        if (currentNodeHeight == null || currentNodeHeight == levels.first()) {
-            return getSendToHomeCmd()
+        return runOnce {
+            currentSetpoint = Setpoint.entries.findLast { it.height < currentSetpoint.height } ?: currentSetpoint
+            io.setElevatorHeightTarget(currentSetpoint.height)
         }
-        
-        val previousLevel = levels.findLast { it.height < currentNodeHeight!!.height }
-        return previousLevel?.let { getSendToNodeCmd(it) } ?: Commands.idle(this)
     }
     
     
     fun getSendToHomeCmd(): Command {
-        currentNodeHeight = null
-        return runOnce { io.setElevatorHeightTarget(0.0.inches) }
+        return getSendToNodeCmd(Setpoint.HOME)
     }
     
     override fun periodic() {
         io.updateInputs(inputs)
         Logger.processInputs(name, inputs)
         
-        Logger.recordOutput("currentNodeHeight", currentNodeHeight.toString())
+        Logger.recordOutput("$name/setpoint", currentSetpoint)
         
         MechanismVisualizer.setElevatorHeight(inputs.carriageHeight)
     }
