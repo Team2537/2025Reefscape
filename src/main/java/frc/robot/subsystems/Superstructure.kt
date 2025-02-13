6687d768f19cf949d832ff342ce118ba77527140
Europa <falon.n.clark@gmail.com>
2025-02-13 18:12:58 -0500

    reef setpoints

--- src/main/java/frc/robot/subsystems/Superstructure.kt
+++ src/main/java/frc/robot/subsystems/Superstructure.kt
@@ -2,23 +2,27 @@ package frc.robot.subsystems
 
 import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.Commands.runOnce
+import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.subsystems.arm.Arm
 import frc.robot.subsystems.elevator.Elevator
+import lib.commands.not
 import lib.math.units.degrees
 import lib.math.units.meters
 import lib.math.units.radians
 import org.littletonrobotics.junction.Logger
+import java.util.function.BooleanSupplier
 import java.util.function.Supplier
 
 class Superstructure {
     val elevator: Elevator = Elevator()
     val arm: Arm = Arm()
-    
+
     private var setpoint: SuperstructureSetpoint = SuperstructureSetpoint.HOME
-    
+
     enum class SuperstructureSetpoint(val elevatorHeight: Distance, val armAngle: Angle) {
         HOME(0.07.meters, 90.0.degrees),
         SOURCE(0.996.meters, (-2.461).radians),
@@ -27,11 +31,18 @@ class Superstructure {
         L3(0.642.meters, 0.862.radians),
         L4(1.289.meters, 0.862.radians),
     }
-    
+
     private val setpointSupplier: Supplier<SuperstructureSetpoint> = Supplier { setpoint }
-    
-    private val reefLevels = listOf(SuperstructureSetpoint.L1, SuperstructureSetpoint.L2, SuperstructureSetpoint.L3, SuperstructureSetpoint.L4)
-    
+
+    private val reefLevels = listOf(
+        SuperstructureSetpoint.L1,
+        SuperstructureSetpoint.L2,
+        SuperstructureSetpoint.L3,
+        SuperstructureSetpoint.L4
+    )
+
+    private val readyToScore: Trigger = Trigger { SmartDashboard.getBoolean("shouldScore", false) }
+
     fun getNextReefNodeCmd(): Command {
         return Commands.sequence(
             runOnce({
@@ -42,12 +53,12 @@ class Superstructure {
                 }
             }),
             Commands.parallel(
-                Commands.defer({elevator.getSendToNodeCmd(setpoint)}, setOf(elevator)),
-                Commands.defer({arm.getSendToNodeCmd(setpoint)}, setOf(arm))
+                Commands.defer({ elevator.getSendToNodeCmd(setpoint) }, setOf(elevator)),
+                Commands.defer({ arm.getSendToNodeCmd(setpoint) }, setOf(arm))
             )
         )
     }
-    
+
     fun getPreviousReefNodeCmd(): Command {
         return Commands.sequence(
             runOnce({
@@ -58,12 +69,12 @@ class Superstructure {
                 }
             }),
             Commands.parallel(
-                Commands.defer({elevator.getSendToNodeCmd(setpoint)}, setOf(elevator)),
-                Commands.defer({arm.getSendToNodeCmd(setpoint)}, setOf(arm))
+                Commands.defer({ elevator.getSendToNodeCmd(setpoint) }, setOf(elevator)),
+                Commands.defer({ arm.getSendToNodeCmd(setpoint) }, setOf(arm))
             )
         )
     }
-    
+
     fun getSendToHomeCmd(): Command {
         return Commands.parallel(
             runOnce({ setpoint = SuperstructureSetpoint.HOME }),
@@ -71,7 +82,36 @@ class Superstructure {
             arm.getSendToNodeCmd(SuperstructureSetpoint.HOME)
         )
     }
-    
+
+    fun getScoreNodeCmd(
+        cancelOverride: Trigger,
+        scoreTrigger: Trigger,
+        isHoldingCoral: BooleanSupplier,
+        node: SuperstructureSetpoint
+    ): Command {
+        if (node == SuperstructureSetpoint.HOME || node == SuperstructureSetpoint.SOURCE) {
+            throw IllegalArgumentException("Can only score at L1-L4")
+        }
+
+        return Commands.sequence(
+            Commands.parallel(
+                runOnce({ setpoint = node }),
+                elevator.getSendToNodeCmd(node),
+                arm.getSendToNodeCmd(node),
+            ),
+            Commands.waitUntil(scoreTrigger.or(cancelOverride)),
+            Commands.either(
+                Commands.sequence(
+                    Commands.print("Outtaking"),
+                    Commands.waitUntil(isHoldingCoral),
+                    getSendToHomeCmd()
+                ),
+                getSendToHomeCmd(),
+                scoreTrigger.and(!cancelOverride)
+            )
+        )
+    }
+
     fun getSendToSourceCmd(): Command {
         return Commands.parallel(
             runOnce({ setpoint = SuperstructureSetpoint.SOURCE }),
@@ -79,7 +119,7 @@ class Superstructure {
             arm.getSendToNodeCmd(SuperstructureSetpoint.SOURCE)
         )
     }
-    
+
     fun periodic() {
         Logger.recordOutput("superstructure/setpoint", setpoint)
     }
