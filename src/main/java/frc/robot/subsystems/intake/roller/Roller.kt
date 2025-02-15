bc31efe9c0deb611b597ef9c4885e657abcff19d
Josie Wedeking <jwedek1007@gmail.com>
2025-02-15 13:00:36 -0800

    attempted (and failed) to create pivot subsystem, gave up once I reached 41 errors.

---
+++ src/main/java/frc/robot/subsystems/intake/roller/Roller.kt
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.intake.roller
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.Constants
+
+class Roller: SubsystemBase() {
+    private val io: IntakeRollerIO = when (Constants.RobotConstants.mode) {
+
+    }
+}
