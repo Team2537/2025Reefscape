9218653112e9ed5299f162307a6feedef0e4adbd
Matthew <84196639+mteam88@users.noreply.github.com>
2025-02-15 10:58:06 -0500

    add skeleton Constants file

---
+++ src/main/java/frc/robot/Constants.kt
@@ -0,0 +1,37 @@
+package frc.robot
+
+object Constants {
+    object RobotConstants {
+        enum class Mode {
+            SIM, REAL, REPLAY
+        }
+    }
+
+    object ArmConstants {
+    
+    }
+
+    object ClimbConstants {
+
+    }
+
+    object ElevatorConstants {
+
+    }
+
+    object GripperConstants {
+
+    }
+
+    object IntakeConstants {
+        object PivotConstants {
+
+        }
+
+        object RollerConstants {
+
+        }
+    }
+
+    // still may need to add constants for other parts of the robot
+}
