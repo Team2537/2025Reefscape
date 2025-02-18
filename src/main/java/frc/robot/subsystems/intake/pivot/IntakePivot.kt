b63450cae531bfde532ad7127ad6cb7995c17e18
Matthew <84196639+mteam88@users.noreply.github.com>
2025-02-18 18:56:30 -0500

    create blank intakepivot subsystem

---
+++ src/main/java/frc/robot/subsystems/intake/pivot/IntakePivot.kt
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.intake.pivot
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.Constants
+import edu.wpi.first.math.system.plant.DCMotor
+import frc.robot.subsystems.intake.pivot.IntakePivotIO.IntakePivotInputs
+import org.littletonrobotics.junction.Logger
+import edu.wpi.first.units.Units.*
+import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.RobotType
+import edu.wpi.first.wpilibj2.command.Commands
+
+class IntakePivot : SubsystemBase() {
+    
+}
