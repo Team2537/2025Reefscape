b6d046c665c6053dc74f64a48a68bbd746d058d7
falon <falon.n.clark@gmail.com>
2025-02-17 23:04:11 -0500

    Remove old superstructure code

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.kt
@@ -26,7 +26,7 @@ class Superstructure {
     val arm: Arm = Arm()
     val gripper: Gripper = Gripper()
     
-    private var setpoint: SuperstructureSetpoint = SuperstructureSetpoint.HOME
+    private var lastRequest: SuperstructureGoal.SuperstructureState = SuperstructureGoal.STOW
     
     val gamepieceSimulator = CoralSimulator(
         { MechanismVisualizer.mechanismPoses[2] },
@@ -34,105 +34,12 @@ class Superstructure {
         Inches.of(24.0)
     )
     
-    enum class SuperstructureSetpoint(val elevatorHeight: Distance, val armAngle: Angle) {
-        HOME(0.07.meters, 90.0.degrees),
-        SOURCE(0.996.meters, (-2.461).radians),
-        L1(0.028.meters, 0.862.radians),
-        L2(0.238.meters, 0.862.radians),
-        L3(0.642.meters, 0.862.radians),
-        L4(1.289.meters, 0.862.radians),
-    }
-    
-    private val setpointSupplier: Supplier<SuperstructureSetpoint> = Supplier { setpoint }
-    
-    private val reefLevels = listOf(
-        SuperstructureSetpoint.L1,
-        SuperstructureSetpoint.L2,
-        SuperstructureSetpoint.L3,
-        SuperstructureSetpoint.L4
-    )
     
     private val readyToScore: Trigger = Trigger { SmartDashboard.getBoolean("shouldScore", false) }
     
-    fun getNextReefNodeCmd(): Command {
-        return Commands.sequence(
-            runOnce({
-                setpoint = when (setpoint) {
-                    SuperstructureSetpoint.L4 -> return@runOnce
-                    in reefLevels -> reefLevels[reefLevels.indexOf(setpoint) + 1]
-                    else -> reefLevels.first()
-                }
-            }),
-            Commands.parallel(
-                Commands.defer({ elevator.getSendToNodeCmd(setpoint) }, setOf(elevator)),
-                Commands.defer({ arm.getSendToNodeCmd(setpoint) }, setOf(arm))
-            )
-        )
-    }
-    
-    fun getPreviousReefNodeCmd(): Command {
-        return Commands.sequence(
-            runOnce({
-                setpoint = when (setpoint) {
-                    SuperstructureSetpoint.L1 -> return@runOnce
-                    in reefLevels -> reefLevels[reefLevels.indexOf(setpoint) - 1]
-                    else -> reefLevels.last()
-                }
-            }),
-            Commands.parallel(
-                Commands.defer({ elevator.getSendToNodeCmd(setpoint) }, setOf(elevator)),
-                Commands.defer({ arm.getSendToNodeCmd(setpoint) }, setOf(arm))
-            )
-        )
-    }
-    
-    fun getSendToHomeCmd(): Command {
-        return Commands.parallel(
-            runOnce({ setpoint = SuperstructureSetpoint.HOME }),
-            elevator.getSendToNodeCmd(SuperstructureSetpoint.HOME),
-            arm.getSendToNodeCmd(SuperstructureSetpoint.HOME)
-        )
-    }
-    
-    fun getScoreNodeCmd(
-        cancelOverride: Trigger,
-        scoreTrigger: Trigger,
-        node: SuperstructureSetpoint
-    ): Command {
-        if (node == SuperstructureSetpoint.HOME || node == SuperstructureSetpoint.SOURCE) {
-            throw IllegalArgumentException("Can only score at L1-L4")
-        }
-        
-        return Commands.sequence(
-            Commands.parallel(
-                runOnce({ setpoint = node }),
-                elevator.getSendToNodeCmd(node),
-                arm.getSendToNodeCmd(node),
-            ),
-            Commands.waitUntil(scoreTrigger.or(cancelOverride)),
-            Commands.either(
-                Commands.sequence(
-                    Commands.print("Outtaking"),
-                    Commands.waitUntil(!gripper.isHoldingCoral),
-                    runOnce({ gamepieceSimulator.score() }),
-                    getSendToHomeCmd()
-                ),
-                getSendToHomeCmd(),
-                scoreTrigger.and(!cancelOverride)
-            )
-        )
-    }
-    
-    fun getSendToSourceCmd(): Command {
-        return Commands.parallel(
-            runOnce({ setpoint = SuperstructureSetpoint.SOURCE }),
-            elevator.getSendToNodeCmd(SuperstructureSetpoint.SOURCE),
-            arm.getSendToNodeCmd(SuperstructureSetpoint.SOURCE)
-        )
-    }
     
     fun periodic() {
-        Logger.recordOutput("superstructure/setpoint", setpoint)
+        Logger.recordOutput("superstructure/setpoint", lastRequest)
         gamepieceSimulator.update()
     }
 }
