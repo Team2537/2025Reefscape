03b9bf932dc1eac846ecb75d154ecf960d3ce317
Matthew <84196639+mteam88@users.noreply.github.com>
2025-02-18 19:16:29 -0500

    add climb subsystem (#70)
    * add climb subsystem
    
    * resolve requested changes
    
    * Refactor climb subsystem voltage and angle calculations

--- src/main/java/frc/robot/Constants.kt
+++ src/main/java/frc/robot/Constants.kt
@@ -2,18 +2,38 @@ package frc.robot
 
 import edu.wpi.first.units.Units.*
 import edu.wpi.first.units.measure.MomentOfInertia
+import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.units.MomentOfInertiaUnit
+import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.units.measure.AngularVelocity
 
 object Constants {
-
 
     object ArmConstants {
     
     }
 
     object ClimbConstants {
+        // PLACEHOLDER VALUES
+        val CLIMB_MOTOR_ID: Int = 1
+        val IS_CLIMB_MOTOR_INVERTED: Boolean = true
+        val CLIMB_GEAR_RATIO: Double = 1.0
+        val MOI: MomentOfInertia = KilogramSquareMeters.of(0.0)
+        val ARM_LENGTH: Distance = Meters.of(0.0)
 
+        // 0 degrees is horizontal, with the end of the climb arm pointing out
+        // positive angle is counterclockwise when viewed as if the climb side of the robot was on the right
+        val RETRACTED_ANGLE: Angle = Degrees.of(250.0)
+        val EXTENDED_ANGLE: Angle = Degrees.of(0.0) // horizontal, with arm pointing to the right
+        val ABSOLUTE_OFFSET: Angle = Degrees.of(0.0) // offset of the absolute encoder from the zero position
+        val EXTEND_VOLTAGE: Voltage = Volts.of(6.0)
+        val RETRACT_VOLTAGE: Voltage = Volts.of(10.0)
+        val EXTEND_ANGLE_TOLERANCE: Angle = Degrees.of(2.0)
+
+        // Retraction detection thresholds
+        val CURRENT_SPIKE_THRESHOLD = Amps.of(30.0)
+        val VELOCITY_ZERO_THRESHOLD = RotationsPerSecond.of(0.1)
     }
 
     object ElevatorConstants {
