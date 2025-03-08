b63b79057ff9be7768a926efd6a3a11e145e7f83
Europa <falon.n.clark@gmail.com>
2025-03-08 16:54:26 -0500

    Update at 'Sat Mar 08 16:54:25 EST 2025'

--- src/main/java/lib/autos/AutoRoutine.kt
+++ src/main/java/lib/autos/AutoRoutine.kt
@@ -49,7 +49,7 @@ class AutoRoutine(
         
         sequence.addCommands(
             Commands.parallel(
-                climb.getSendToPositionCommand{ (60).degrees }.onlyIf{RobotBase.isReal()},
+                climb.getSendToPositionCommand{ (60).degrees }.onlyIf{RobotBase.isReal()}.withTimeout(2.5),
                 Commands.sequence(
                     superstructure.getStowCommand(),
                     AutoBuilder.resetOdom(startPath.startingHolonomicPose.getOrDefault(Pose2d())),
