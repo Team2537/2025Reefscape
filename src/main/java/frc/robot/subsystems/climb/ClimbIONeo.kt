bae904aeae43d5753a71d83aedf99f06e055a376
Europa <falon.n.clark@gmail.com>
2025-03-08 14:57:12 -0500

    Update at 'Sat Mar 08 14:57:12 EST 2025'

--- src/main/java/frc/robot/subsystems/climb/ClimbIONeo.kt
+++ src/main/java/frc/robot/subsystems/climb/ClimbIONeo.kt
@@ -26,7 +26,7 @@ class ClimbIONeo(
         smartCurrentLimit(60)
     }
 
-    private val throughbore: DutyCycleEncoder = DutyCycleEncoder(encoderID, 1.0, 0.58)
+    private val throughbore: DutyCycleEncoder = DutyCycleEncoder(encoderID, 1.0, 2.439)
 
     private val motor = SparkMax(id, SparkLowLevel.MotorType.kBrushless).apply {
         configure(coastConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
