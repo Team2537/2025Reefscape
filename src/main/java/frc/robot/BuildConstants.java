9603ac8ce502fb3703b054e00bddb5620b8478e6
Matthew <84196639+mteam88@users.noreply.github.com>
2025-11-06 18:40:49 -0500

    retune

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 301;
-  public static final String GIT_SHA = "5de5c571a94aa6d13c82b15caa26a1902c3b3876";
-  public static final String GIT_DATE = "2025-11-04 18:13:16 EST";
+  public static final int GIT_REVISION = 303;
+  public static final String GIT_SHA = "f66458f6ff34eb31cc5c2f8f7e9bb3d8b2532e7c";
+  public static final String GIT_DATE = "2025-11-06 18:27:55 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-11-04 18:41:41 EST";
-  public static final long BUILD_UNIX_TIME = 1762299701653L;
+  public static final String BUILD_DATE = "2025-11-06 18:39:50 EST";
+  public static final long BUILD_UNIX_TIME = 1762472390649L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
